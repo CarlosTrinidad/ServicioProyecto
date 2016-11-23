7b6770ea1771bf6dfa0d913d85adaf1e82ddad74
Cattorres <torres.tinal@hotmail.com>
2016-11-22 18:54:30 -0600

    Update view.php

--- views/subject-semester/view.php
+++ views/subject-semester/view.php
@@ -15,6 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
+        <?= Html::a('Create Subject Semester', ['create'], ['class' => 'btn btn-success']) ?>
         <?= Html::a('Update', ['update', 'id' => $model->semester_id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->semester_id], [
             'class' => 'btn btn-danger',
