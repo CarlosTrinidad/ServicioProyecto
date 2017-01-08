bdc90c87ec44d21c5d5172286e98b2c37eff4579
Luis Casanova <luis.casanova.uady@live.com>
2017-01-08 12:41:34 -0600

    Se agrega botón horario en vista semestre

--- views/semester/index.php
+++ views/semester/index.php
@@ -30,7 +30,13 @@ $this->params['breadcrumbs'][] = $this->title;
 				'value' => 'studyProgram.name',
 				'header' => 'Study Program',
 			],
-            ['class' => 'yii\grid\ActionColumn'],
+             ['class' => 'yii\grid\ActionColumn',
+             'template' =>'{view} {update} {delete} {schedule}',
+             'buttons' => [
+             'schedule' => function ($url, $model, $key) {
+                return Html::a('<span class="glyphicon glyphicon-calendar"></span>', ['semesterschedule/semester', 'id' => $model->id],[ 'title' => Yii::t('app', 'Schedule'),'aria-label'=>Yii::t('app', 'Schedule'),'data-pjax' => "0",]);
+            },],
+            ],
         ],
     ]); ?>
 </div>
