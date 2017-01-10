0d99f8c0bc3f6e716fed2288d3867c85921c3b32
CarlosTrinidad <trinidad.carlos@hotmail.com>
2017-01-09 23:14:38 -0600

    controlador para vista de subject

---
+++ controllers/SubjectscheduleController.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\controllers;
+use app\models\Instructor;
+use app\models\Subject;
+use app\models\Classes;
+use yii\web\Controller;
+use app\models\Schedule;
+use app\models\Semester;
+use yii\helpers\Url;
+
+class SubjectscheduleController extends Controller
+{
+
+	public function actionSubject($id){
+    echo $id;
+		\Yii::$app->session->set('returnUrl', Url::to(['subjectschedule/subject', 'id'=>$id]));
+
+    $interval = Schedule::find()->all();
+    $subj = Subject::findOne($id);
+        return $this->render("subjectSchedule",["interval"=>$interval,"subject"=>$subj]);
+	}
+}
