ecf484431a78b49ea5491290ebd68a19a7191662
Luis Casanova <luis.casanova.uady@live.com>
2017-01-11 15:09:25 -0600

    Se agrega botón horario study program

--- views/study-program/index.php
+++ views/study-program/index.php
@@ -27,7 +27,13 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'name',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            ['class' => 'yii\grid\ActionColumn',
+             'template' =>'{view} {update} {delete} {schedule}',
+             'buttons' => [
+             'schedule' => function ($url, $model, $key) {
+                return Html::a('<span class="glyphicon glyphicon-calendar"></span>', ['study-program/schedule', 'id' => $model->id],[ 'title' => Yii::t('app', 'Schedule'),'aria-label'=>Yii::t('app', 'Schedule'),'data-pjax' => "0",]);
+            },],
+            ],
         ],
     ]); ?>
 </div>
