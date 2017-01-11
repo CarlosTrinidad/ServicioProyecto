9725bc054e6441a085d6b86a6a94d85da61b85ab
Luis Casanova <luis.casanova.uady@live.com>
2017-01-10 22:29:40 -0600

    Se modifica controlador Rooms

--- controllers/RoomscheduleController.php
+++ controllers/RoomscheduleController.php
@@ -20,4 +20,13 @@ class RoomscheduleController extends Controller
     $room = Room::findOne($id);
         return $this->render("roomSchedule",["interval"=>$interval,"room"=>$room]);
 	}
+
+
+	public function actionRooms(){
+		\Yii::$app->session->set('returnUrl', Url::to(['roomschedule/rooms']));
+
+    $interval = Schedule::find()->all();
+    $rooms = Room::find()->all();
+        return $this->render("roomGroupSchedule",["interval"=>$interval,"rooms"=>$rooms]);
+	}
 }
